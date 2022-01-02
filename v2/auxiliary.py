26994b7754fe2cca505f29d7a2134abba840d97c
Christopher Nguyen <chrisngyn99@gmail.com>
2022-01-02 13:37:16 -0500

    jesus christ i need to learn how to sync a replit repo with a github repo

--- v2/auxiliary.py
+++ v2/auxiliary.py
@@ -1,7 +1,7 @@
 import heapq
 from replit import db
 
-async def top_users(bot, ctx):  # has to be a better way than passing stuff around all the time right
+async def top_users(bot, ctx):  # has to be a better way than passing stuff around all the time
     min_heap = []
 
     i = 0
@@ -11,10 +11,13 @@ async def top_users(bot, ctx):  # has to be a better way than passing stuff arou
 
         if len(min_heap) > 10:
             heapq.heappop(min_heap)
-    
+
     msg = ''
 
-    for count, _, user_id in min_heap:
-        msg += f'{i} - {await bot.fetch_user(user_id)} with count of {count}'
-    
+    j = len(min_heap)
+    while min_heap:
+        count, _, user_id = heapq.heappop(min_heap)
+        msg = f'{j}. {await bot.fetch_user(user_id)} with a count of {count} \n' + msg
+        j -= 1
+
     return msg
